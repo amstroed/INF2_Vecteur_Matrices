ee19909e3233abc7c0f97c2516655c418af2d471
Teo <ferraricorneloup.teo@gmail.com>
2019-04-02 16:51:07 +0200

    gitignore

---
+++ src/Labo4.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Labo4.cpp
+// Author      : Teo Ferrari
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
