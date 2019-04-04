2bfe793f459a7b0c08a1c08217c45e09fffb335f
Loïc <loic.geinoz@gmail.com>
2019-04-04 16:25:15 +0200

    Modification et implementation de vecteur (devrait être pas mal)

--- src/Vecteur.h
+++ src/Vecteur.h
@@ -4,24 +4,36 @@
 #include <vector>
 #include <iostream>
 
+template <typename T> class Vecteur;
+template <typename T>
+std::ostream & operator<< (std::ostream & os, const Vecteur<T>& vect)
+{
+	vect.contenuVecteur.at(0);
+}
+
 template <typename T>
 class Vecteur
 {
-	friend std::ostream& operator <<(std::ostream& os, const Vecteur<T>& vect);
+	friend std::ostream& operator<< <T>(std::ostream& os, const Vecteur<T>& vect);
 
 public :
-	Vecteur(vector<T>)
+	Vecteur(vector<T> vect)
 	{
-
+		contenuVecteur = vect;
+		capaciteVecteur = vect.capacity();
+		tailleVecteur = vect.size();
 	}
 	Vecteur(size_t n)
 	{
-
+		capaciteVecteur = n;
+		tailleVecteur = 0;
 	}
 
-	T at(size_t n);
+	T& at(size_t n);
+	T at(size_t n) const;
 	void resize(size_t size);
-	T somme();
+	T somme() const;
+	unsigned size();
 
 	Vecteur<T> operator*(T valeur);
 	Vecteur<T> operator*(Vecteur vect);
@@ -30,6 +42,8 @@ public :
 
 private :
 	vector<T> contenuVecteur;
+	unsigned capaciteVecteur;
+	unsigned tailleVecteur;
 };
 #include "VecteurImpl.h"
 #endif // VECTEUR_H
--- src/VecteurImpl.h
+++ src/VecteurImpl.h
@@ -8,44 +8,102 @@
 using namespace std;
 
 template <typename T>
-Vecteur<T> at(unsigned n)
+T& Vecteur<T>::at(unsigned n)
 {
-
+	return this->contenuVecteur.at(n);
 }
 
-void resize(size_t size)
+template <typename T>
+T Vecteur<T>::at(size_t n) const
 {
 
 }
 
 template <typename T>
-T somme()
+void Vecteur<T>::resize(size_t nouvelleCapacite)
 {
+	if (nouvelleCapacite < capaciteVecteur)
+	{
+		//A definir
+		//throw();
+	}
+	this->capaciteVecteur = size;
+}
 
+template <typename T>
+unsigned Vecteur<T>::size()
+{
+	return this->tailleVecteur;
+}
+
+template <typename T>
+T Vecteur<T>::somme() const
+{
+	T somme = T();
+	for (size_t i = 0; i < this->tailleVecteur; ++i)
+	{
+		somme += this->contenuVecteur.at(i);
+	}
+	return somme;
 }
 
 template <typename T>
 Vecteur<T> Vecteur<T>::operator*(T valeur)
 {
-
+	for (size_t i = 0; i < this->tailleVecteur; ++i)
+	{
+		this->contenuVecteur.at(i) *= valeur;
+	}
 }
 
 template <typename T>
 Vecteur<T> Vecteur<T>::operator * (Vecteur vect)
 {
-
+	if (vect.taille != this->taille)
+	{
+		//a definir
+		//throw();
+	}
+	else
+	{
+		for (size_t i = 0; i < this->tailleVecteur; ++i)
+		{
+			this->contenuVecteur.at(i) *= vect.contenuVecteur.at(i);
+		}
+	}
 }
 template <typename T>
 Vecteur<T> Vecteur<T>::operator + (Vecteur vect)
 {
-
+	if (vect.taille != this->taille)
+	{
+		//a definir
+		//throw();
+	}
+	else
+	{
+		for (size_t i = 0; i < this->tailleVecteur; ++i)
+		{
+			this->contenuVecteur.at(i) += vect.contenuVecteur.at(i);
+		}
+	}
 }
 
 template <typename T>
 Vecteur<T> Vecteur<T>::operator - (Vecteur vect)
 {
-
+	if (vect.taille != this->taille)
+	{
+		//a definir
+		//throw();
+	}
+	else
+	{
+		for (size_t i = 0; i < this->tailleVecteur; ++i)
+		{
+			this->contenuVecteur.at(i) -= vect.contenuVecteur.at(i);
+		}
+	}
 }
-
 
 #endif // VECTEURIMPL_H
